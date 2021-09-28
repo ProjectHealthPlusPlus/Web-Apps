eba5af0b3e424102999ce1de1a7b8f3166888605
DiegoHZ5 <safairus@gmail.com>
2021-09-28 14:04:31 -0500

    feat: new components
    some general components

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,36 @@
     <v-main>
       <HelloWorld/>
       <HelloWorld/>
+<!--      <app-register/>-->
+<!--      <app-login/>-->
+      <app-account-settings/>
     </v-main>
     <AppFooter/>
   </v-app>
 </template>
 
+<!--  <template>-->
+<!--    <app-login></app-login>-->
+<!--  </template>-->
+
 <script>
 import HelloWorld from './components/HelloWorld';
 import AppHeader from "./layout/AppHeader";
 import AppFooter from './layout/AppFooter';
+import AppAccountSettings from "./components/common/account-settings/AccountSettings";
+// import AppLogin from "./views/pages/Login";
+// import AppRegister from "./views/pages/Register";
 
 export default {
   name: 'App',
 
   components: {
+    AppAccountSettings,
+    // AppRegister,
+    // AppLogin,
     HelloWorld,
     AppHeader,
-    AppFooter
+    AppFooter,
   },
-
 };
 </script>
